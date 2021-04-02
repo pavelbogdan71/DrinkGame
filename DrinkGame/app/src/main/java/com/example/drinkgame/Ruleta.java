7ea133968f746e26e77c8ebd62bcd22167349464
Pavel Bogdan <pavelbogdan71@gmail.com>
2021-04-02 18:27:24 +0300

    spin worksss
    am facut sa mearga spinul

--- DrinkGame/app/src/main/java/com/example/drinkgame/Ruleta.java
+++ DrinkGame/app/src/main/java/com/example/drinkgame/Ruleta.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,5 +29,43 @@ public class Ruleta extends AppCompatActivity {
         ArrayAdapter<Player> adapter = new ArrayAdapter<Player>(this, android.R.layout.simple_list_item_1,players);
         listView.setAdapter(adapter);
     }
+
+
+    public void spinWheel(View view) throws InterruptedException {
+            Intent intent = getIntent();
+            ArrayList<Player> players = intent.getParcelableArrayListExtra("players");
+            ArrayAdapter<Player> adapter = new ArrayAdapter<Player>(this, android.R.layout.simple_list_item_1,players);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                int i=0;
+                @Override
+                public void run() {
+                    if(i <10)
+                    {
+                        Player firstPlayer = players.get(0);
+                        adapter.remove(firstPlayer);
+                        adapter.add(firstPlayer);
+                        players.remove(firstPlayer);
+                        players.add(firstPlayer);
+
+                        adapter.notifyDataSetChanged();
+                        handler.postDelayed(this, 100);
+
+                        i++;
+                    }else{
+                        handler.removeCallbacks(this);
+                    }
+
+                }
+                }, 1000);
+
+                listView.setAdapter(adapter);
+
+
+
+
+    }
+
 
 }
